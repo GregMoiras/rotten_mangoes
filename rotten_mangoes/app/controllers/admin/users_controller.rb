d7af232c51f2c6985a2a6264f217f305ee80bd9e
GregMiras <gregmoiras@gmail.com>
2014-09-02 18:51:39 +0000

    added admin functionality to view,create,edit users

--- rotten_mangoes/app/controllers/admin/users_controller.rb
+++ rotten_mangoes/app/controllers/admin/users_controller.rb
@@ -1,10 +1,48 @@
 class Admin::UsersController < ApplicationController
 
   before_action :must_be_admin
+
   def index
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "#{@user.firstname} #{@user.lastname} was created successfully!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
 
   private
   
@@ -13,6 +51,10 @@ class Admin::UsersController < ApplicationController
       @user = User.find(session[:user_id])
       redirect_to movies_path, notice: "This page is accessible only by Admins #{@user.firstname}!" unless @user.admin == true
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
   end
 
 end
